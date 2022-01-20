891bbcf0662299c7542603737e42bd6f5b5c9025
Logan Campbell <colin@logaan.net>
2022-01-21 01:10:01 +1100

    printing turing table

--- src/turing_machine/core.cljs
+++ src/turing_machine/core.cljs
@@ -6,15 +6,58 @@
 
 (enable-console-print!)
 
+(defprotocol Humanize
+  (humanize [self]))
+
+(defrecord Step [tape direction state]
+  Humanize
+  (humanize [_]
+    (str tape ({:left "L" :right "R"} direction) state)))
+
+(defrecord Halt []
+  Humanize
+  (humanize [_] "HALT"))
+
+;; https://en.wikipedia.org/wiki/Turing_machine
+;; Using strings here in anticipation of human user input
 (def initial-state
-  {:text "Hello world!"})
+  {:states ["0" "1" "2" "3"]
+   :tape-symbols ["0", "1"]
+   :blank-symbol "0"
+   :initial-state ["0"]
+   :state-table
+   ;; {state {tape [tape, direction, state]}}
+   {"0" {"0" (Step. "1" :left "1")
+         "1" (Step. "1" :right "0")}
+    "1" {"0" (Step. "1" :right "2")
+         "1" (Step. "0" :left "2")}
+    "2" {"0" (Step. "1" :right "3")
+         "1" (Step. "0" :left "0")}
+    "3" {"0" (Halt.)
+         "1" (Halt.)}}})
 
 (defonce app-state (atom initial-state))
 
 (defn hello-world [state]
-  (sab/html [:div
-             [:h1 (:text @state)]
-             [:h3 "Edit this and watch it change!"]]))
+  (let [snapshot @state
+        table (:state-table snapshot)]
+    (sab/html
+     [:div
+      [:h1 "Turing machine"]
+      [:table
+       [:thead
+        [:tr
+         [:th " "]
+         (for [symbol (:tape-symbols snapshot)]
+           [:th {:key symbol} symbol])]]
+       [:tbody
+        (for [state (:states snapshot)]
+          [:tr {:key state}
+           [:td state]
+           (for [symbol (:tape-symbols snapshot)]
+             (let [cell (get-in table [state symbol])]
+               [:td {:key (str state "-" symbol)}
+                (humanize cell)]))])]]])))
 
 (defn on-js-reload []
   (reset! app-state initial-state))
